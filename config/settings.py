6e3ededbad3c8e94ff5ffa8992b1ac03336fd48a
philastrio <han_gang@naver.com>
2019-11-09 19:53:01 +0900

    5.0시작

--- config/settings.py
+++ config/settings.py
@@ -43,8 +43,9 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
 ]
+THIRD_PARTY_APPS = ["django_countries"]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Room)
-class RoomAdmin(admin.ModelAdmin):
+@admin.register(models.RoomType, models.Facility, models.AmenityType, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """ Item Admin Definition """
+
     pass
 
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    """ Item Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """  """
+
+    pass
