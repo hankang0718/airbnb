f97073b9eb60a00f12eb990461dcdba94c509784
philastrio <han_gang@naver.com>
2019-11-10 09:00:19 +0900

    6.0시작

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+
+class Reservation(core_models.TimeStampedModel):
+    """ Reservation Model Definition """
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "pending"),
+        (STATUS_CONFIRMED, "confirmed"),
+        (STATUS_CANCELED, "canceled"),
+    )
+
+    status = models.CharField(max_length=12, default=STATUS_PENDING)
+    check_in = models.DateField(blank=True, null=True)
+    check_out = models.DateField(blank=True, null=True)
+    guest = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, blank=True, null=True
+    )
+    room = models.ForeignKey(
+        "rooms.Room", on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.room} - {self.check_in}"
+
