6ff87149bce9bc10d910f80ed3f11388d409b778
philastrio <han_gang@naver.com>
2019-12-04 06:13:40 +0900

    class 기반 Search 만들기

--- rooms/models.py
+++ rooms/models.py
@@ -73,7 +73,7 @@ class Room(core_models.TimeStampedModel):
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
-    guests = models.IntegerField()
+    guests = models.IntegerField(help_text="얼마나 많은 손님이 주무실거에요?")
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
