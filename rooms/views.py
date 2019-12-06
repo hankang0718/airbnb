7743b4493927cc6a9ada15cef7da8ed4f49b2f1b
philastrio <han_gang@naver.com>
2019-12-07 05:04:59 +0900

    Login 만들기

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
-from django_countries import countries
+from django.core.paginator import Paginator
 from . import models, forms
 
 
@@ -28,7 +28,9 @@ class SearchView(View):
         country = request.GET.get("country")
 
         if country:
+
             form = forms.SearchForm(request.GET)
+
             if form.is_valid():
                 city = form.cleaned_data.get("city")
                 country = form.cleaned_data.get("country")
@@ -80,9 +82,18 @@ class SearchView(View):
                 for facility in facilities:
                     filter_args["facilities"] = facility
 
-                rooms = models.Room.objects.filter(**filter_args)
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get("page", 1)
+
+                rooms = paginator.get_page(page)
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
 
         else:
             form = forms.SearchForm()
 
-        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+        return render(request, "rooms/search.html", {"form": form})
